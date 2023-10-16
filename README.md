fa3c0eca1e517a3110ce410ee1f9e34969734778
slaney <93399147+slaneycoursera@users.noreply.github.com>
2023-10-16 09:39:27 -0600

    MDMIssuesReport added

--- README.md
+++ README.md
@@ -1,70 +1,136 @@
 # Jamf Scripts
 
+  
+
 This repository contains a collection of scripts designed to be used with Jamf management for macOS devices. These scripts help manage various aspects of macOS environments, such as delaying policy executions, managing power assertions, and submitting log files to IT.
+
+  
 
 Some of the scripts in this repository, like the Log Collection and Submission script, require encrypted passwords or tokens to authenticate and access APIs or protected resources. These encrypted tokens need to be decrypted before they can be utilized in the script.
 
+  
+
 ## DecryptString Function Usage
+
 To handle the decryption, the `DecryptString` function is implemented in the scripts, allowing secure storage and usage of sensitive information. The function accepts three arguments:
 
+  
+
 - Encrypted String: The encrypted token or password to be decrypted
+
 - Salt: A random data value generated during encryption to ensure uniqueness and strengthen the security of the encrypted data
+
 - Passphrase: A secret key or password used during encryption and decryption process
+
+  
 
 Here's an example of how the function is used in a script:
 
+  
+
 bash
+
+  
 
 Copy code
 
+  
+
 ```bash
+
 workatoApiToken=$(DecryptString $4 $5 $6)
+
 ```
+
+  
 
 In this example,5, and $6 are the command-line arguments that need to be passed for the encrypted string, salt, and passphrase, respectively.
 
+  
+
 When deploying the scripts, be sure to supply the correct encrypted string, salt, and passphrase as command-line arguments, ensuring secure decryption and use of sensitive tokens or passwords within the script.
 
+  
+
 ## Available Scripts
+
+  
 
 1. [jamfLocalPolicyDelay.sh](https://github.com/feolaney/Jamf-Scripts/blob/main/jamfLocalPolicyDelay.sh)
 2. [selfServiceLogSubmissionTool.sh](https://github.com/feolaney/Jamf-Scripts/blob/main/Scripts/selfSerivceLogSubmissionTool.sh)
 3. [updateAliasBasedOnUserName.sh](https://github.com/feolaney/Jamf-Scripts/blob/main/Scripts/updateAliasBasedOnUserName.sh)
+4. [MDMIssuesReport.sh]()
+
+  
 
 ## Script Descriptions
+
 ****
+
 ### Power Assertion Management
+
+  
 
 This script is used to manage power assertions in macOS environments with Jamf management, enabling a delay in Jamf policy executions. It takes Jamf Custom Event Trigger and a set of assertions to ignore as inputs and performs the following main operations:
 
+  
+
 - Checks for active power assertions that shouldn't be ignored
+
 - Triggers a Jamf policy event if an active power assertion matches ignored assertions
+
 - Creates a new script and Launch Daemon if active power assertions are found, running the script every 15 minutes
 
+  
+
 This script minimizes disruptions to users while managing power assertions, for example, during video calls or when specific applications require an active display.
+
 ****
+
 ### Log Collection and Submission
+
+  
 
 This script is designed to collect and submit specified log files to IT in macOS environments. It allows users to select the type of log files they want to submit, processes the logs, and performs the operations:
 
+  
+
 - Creates a dialog box for users to select the log types to submit
+
 - Defines and processes various log types (e.g., GlobalProtect, Jamf Connect Logs, and Sysdiagnose Logs)
+
 - Collects, creates, and uploads log files to an AWS S3 bucket
+
 - Notifies IT via Slack when log files are submitted successfully or if there are any errors
+
+  
 
 This script facilitates log file management by allowing simple log submission from users to IT through a friendly interface.
 
+  
+
 ==**Homework for the user**==
+
 The script provided was initially created with the help of an automation service that generated the AWS PreSigned URL using an AWS Lambda Function. This approach was used to securely generate the AWS Upload URL off the local device, avoiding the need to share API credentials. In order for this script to be effectively adopted and used, this function may need to be replicated or replaced to suit your specific needs.
+
+  
 
 The `generateS3BucketURL` function is responsible for initiating the creation of the AWS PreSigned URL. It was observed that the URL generation process took between 1 to 3 minutes when using the automation API. To account for this delay, a UUID was returned by the function, which could then be supplied to the automation resource to collect the URL when finished.
 
+  
+
 This process can be seen in the `generateS3BucketURL` function, where the UUID is collected. Then, within the locally created script from lines 282-307, the UUID is used to check if the URL has been generated. The script will periodically check for the URL generation before proceeding to upload the logs.
+
+  
 
 For more information on AWS Lambda Function invocations and related API documentation, refer to the following resources:
 
+  
+
 - [Invoking AWS Lambda Functions](https://docs.aws.amazon.com/lambda/latest/dg/urls-invocation.html)
+
 - [Invoke in AWS Lambda API Documentation](https://docs.aws.amazon.com/lambda/latest/dg/API_Invoke.html)
+
 ****
 ### Local Alias Creation
 Alias Creation Based on Asset Inventory System This script is used to add a macOS local user alias based on information pulled from an external asset inventory system. The script performs the following main operations:
@@ -75,3 +141,24 @@ Alias Creation Based on Asset Inventory System This script is used to add a macO
 - If not, sets the alias for the current user
 
 This script automates the process of alias creation for macOS users, simplifying user management in systems integrated with external asset inventory systems.
+
+****
+
+###MDM Failed Errors Search
+
+  
+
+This script is designed to interact with the Jamf API, to perform a series of checks on a group of computers managed by Jamf Pro, and to identify computers with specific status messages. It takes a list of status messages and smart group's ID as input, checks the command history for each computer within the smart group, and identifies computers with a history item containing any of the specified status messages. The script provides a summary of the IDs of computers, for each status message, along with respective URLs to access the records of the computers on the Jamf Pro web interface.
+
+Script could run for a while if the search smart group is large
+
+**Variables to Set:**
+
+- `apiUser`: Set this variable to the username of the Jamf Pro API user. This is required for making API requests.
+    
+- `jssApiPassword`: Set this variable to the password for the Jamf Pro API user. This is also required for making API requests.
+    
+- `smartGroupId`: Set this variable to the ID number of the smart group in your Jamf Pro system. All computers within this group will be checked.
+    
+- `statusTexts`: This variable is an array of status messages. The script will attempt to find these statuses in each computer's command history. Set the statuses according to the issues you want to identify. For example, a status message might indicate a problem with MDM communication or that a required configuration profile is missing.
+	- Example: `statusTexts=("The device token is not active for the specified topic." "Another status message")`
---
+++ Scripts/MDMIssuesReport.sh
@@ -0,0 +1,236 @@
+#!/bin/zsh
+
+################
+################
+# JAMF API to set Name Change Variable to No complete with authentication process
+################
+################
+
+# Define the list of status texts to search for
+#  statusTexts=("The device token is not active for the specified topic." "Another status message")
+
+# 'statusTexts' is an array of status texts. These status messages will be searched for in each computer's history.
+# Any computer with a history item whose status matches any of these texts will have its ID added to a list.
+# For example, "The device token is not active for the specified topic." status might indicate that the computer
+# has a problem with MDM communication. 
+#
+# Here is an example of statusTexts:
+# statusTexts=("The device token is not active for the specified topic." "Another status message")
+
+typeset -a statusTexts=("The device token is not active for the specified topic." "Push using revoked certificate." "Update to MDM profile contains different server URL." "The SCEP server")
+
+# The 'jssApiPassword' variable should contain the password for your JAMF API user. 
+# The 'apiUser' variable should contain the username for your JAMF API user.
+
+# The following variables can be set to hardcode credentials for authentication when making API requests.
+
+jssApiPassword=""
+apiUser=""
+
+# The 'smartGroupId' variable should contain the ID number of the JAMF smart group that you're checking.
+
+smartGroupId=""
+
+
+# Script Variables
+
+curlOptions=(--silent --show-error --connect-timeout 60 --fail)
+
+myJssBaseurl=$( /usr/bin/defaults read /Library/Preferences/com.jamfsoftware.jamf jss_url )
+myJssApiurl="${myJssBaseurl}api/v1/jamf-management-framework/redeploy/"
+#
+#
+#
+# FUNCTIONS
+#
+#
+#
+
+GetJamfProAPIToken() {
+
+# This function uses Basic Authentication to get a new bearer token for API authentication.
+echo "Getting Jamf API token"
+authtoken=$(curl "${curlOptions[@]}" -k -u "${apiUser}:${jssApiPassword}" -X POST "${myJssBaseurl}/uapi/auth/tokens" -H "accept: application/json")
+# Parse the token from the response using awk
+jamfApiToken=$(echo "$authtoken" | awk -F '"' '{for(i=1;i<=NF;i++)if($i=="token")print $(i+2)}')
+
+}
+
+APITokenValidCheck() {
+
+# Verify that API authentication is using a valid token by running an API command
+# which displays the authorization details associated with the current API user. 
+# The API call will only return the HTTP status code.
+apiAuthenticationCheck=$(curl --write-out "%{http_code}" --silent --output /dev/null "${myJssBaseurl}/api/v1/auth" -X GET -H "Authorization: Bearer ${jamfApiToken}")
+}
+
+CheckAndRenewAPIToken() {
+
+# If the api_authentication_check has a value of 200, that means that the current
+# bearer token is valid and can be used to authenticate an API call.
+APITokenValidCheck
+if [[ ${apiAuthenticationCheck} == 200 ]]; then
+
+# If the current bearer token is valid, it is used to connect to the keep-alive endpoint. This will
+# trigger the issuing of a new bearer token and the invalidation of the previous one.
+#
+# The output is parsed for the bearer token and the bearer token is stored as a variable.
+  authtoken=$(curl "${curlOptions[@]}" -H "Authorization: Bearer ${jamfApiToken}" -X POST "${myJssBaseurl}/api/v1/auth/keep-alive" -H "accept: application/json")
+  # authtoken=$(/usr/bin/curl "${myJssBaseurl}/api/v1/auth/keep-alive" -silent -request POST -header "Authorization: Bearer ${jamfApiToken}")
+  jamfApiToken=$(echo "$authtoken" | awk -F '"' '{for(i=1;i<=NF;i++)if($i=="token")print $(i+2)}')
+else
+
+# If the current bearer token is not valid, this will trigger the issuing of a new bearer token
+# using Basic Authentication.
+  echo "Getting new token"
+   GetJamfProAPIToken
+fi
+}
+InvalidateToken() {
+
+# Verify that API authentication is using a valid token by running an API command
+# which displays the authorization details associated with the current API user. 
+# The API call will only return the HTTP status code.
+
+APITokenValidCheck
+
+# If the api_authentication_check has a value of 200, that means that the current
+# bearer token is valid and can be used to authenticate an API call.
+
+if [[ ${apiAuthenticationCheck} == 200 ]]; then
+
+# If the current bearer token is valid, an API call is sent to invalidate the token.
+
+      authtoken=$(/usr/bin/curl "${myJssBaseurl}/api/v1/auth/invalidate-token" -silent  -header "Authorization: Bearer ${jamfApiToken}" -X POST)
+      
+# Explicitly set value for the api_token variable to null.
+
+      jamfApiToken=""
+
+fi
+}
+
+
+#
+#
+#
+# MAIN SCRIPT
+#
+#
+#
+
+GetJamfProAPIToken
+################
+# Beaer Token setup
+################
+
+
+# If jssApiPassword or apiUser is not set, prompt the user for these values
+if [[ -z "${jssApiPassword// }" ]]; then
+  echo "Please enter the jssApiPassword:"
+  read jssApiPassword
+fi
+
+if [[ -z "${apiUser// }" ]]; then
+  echo "Please enter the apiUser:"
+  read apiUser
+fi
+
+if [[ -z "${smartGroupId// }" ]]; then
+  echo "Please enter the smartGroupId:"
+  read smartGroupId
+fi
+
+
+authtoken=$(curl "${curlOptions[@]}" -k -u "${apiUser}:${jssApiPassword}" -X POST "${myJssBaseurl}/uapi/auth/tokens" -H "accept: application/json")
+# Parse the token from the response using awk
+jamfApiToken=$(echo "$authtoken" | awk -F '"' '{for(i=1;i<=NF;i++)if($i=="token")print $(i+2)}')
+
+
+# An associative array to hold the IDs of computers with each status from statusTexts
+typeset -A computersWithStatus
+for statusMessage in "${statusTexts[@]}"; do
+  computersWithStatus[$statusMessage]=""
+done
+
+# Make a request to the API endpoint that will return information about our smart group
+smartGroupResponse=$(curl "${curlOptions[@]}" -k -H "Authorization: Bearer ${jamfApiToken}" -X GET "${myJssBaseurl}/JSSResource/computergroups/id/${smartGroupId}")
+
+# Save the response in a temporary xml file
+echo "${smartGroupResponse}" > temp.xml
+
+# Parse the XML with xmllint to grab the computer ids and store them in a variable
+computerIds=$(xmllint --xpath '//*[local-name()="computer_group"]/*[local-name()="computers"]/*[local-name()="computer"]/*[local-name()="id"]/text()' temp.xml 2>/dev/null)
+
+# Display first 20 elements of the array
+
+rm temp.xml
+# The specified status text to search for
+
+# An array to hold the IDs of computers with MDM issues
+declare -a mdmIssueComputers=()
+
+
+# Convert computerIds to an array
+computerIds_ARR=()
+for id in ${(ps:\n:)computerIds}; do
+  computerIds_ARR+=("$id")
+done
+
+echo "Searching for all computers in Smart Group ID: $smartGroupId"
+
+count=0
+
+# Iterate over each computer ID
+for computerId in "${computerIds_ARR[@]}"; do
+  count=$((count + 1))
+  # Make an API request to fetch the history of the given computer
+  computerHistoryResponse=$(curl ${curlOptions} -k -H "Authorization: Bearer ${jamfApiToken}" -X GET "${myJssBaseurl}/JSSResource/computerhistory/id/${computerId}")
+  echo -n "Searching ID: $computerId"
+  curlExitStatus=$?
+  
+  if [[ ${curlExitStatus} -eq 22 ]]; then
+    echo "Computer not found for ID: ${computerId}, HTTP 401 error encountered."
+    continue
+  fi
+
+  # Save the response in a temporary xml file
+  echo "${computerHistoryResponse}" > temp.xml
+  
+  # Check each status text
+  for statusText in "${statusTexts[@]}"; do
+  # Grab the command statuses
+  commandStatuses=$(xmllint --xpath '//computer_history/commands/failed/command/status/text()' temp.xml 2>/dev/null)
+  
+  # If this status text was found in the command statuses
+  if [[ ${commandStatuses} == *${statusText}* ]]; then
+      echo -n " - Found status: $statusText"
+      
+      # Add this computer ID to the correct array in our associative array
+      computersWithStatus[$statusText]+="$computerId "
+  fi
+  done
+    # Remove the temporary xml file
+  rm temp.xml
+  echo ""
+
+  # if 100 records have been searched renew API Token
+  if ((count % 100 == 0)); then
+    echo "\\nChecking API token\\n"
+    CheckAndRenewAPIToken
+  fi
+
+done
+
+# Print out the list of IDs for each status for final confirmation
+for statusMessage in "${statusTexts[@]}"; do
+  echo "\\n-------\\n-------\\nList of computer IDs with issue '${statusMessage}':"
+  echo ${computersWithStatus[$statusMessage]}
+  echo "\\nURLs:"
+  for computerId in ${(ps: :)computersWithStatus[$statusMessage]}
+    do
+        echo "${myJssBaseurl}computers.html?id=${computerId}&o=r"
+  done
+  echo ""
+done
+InvalidateToken
