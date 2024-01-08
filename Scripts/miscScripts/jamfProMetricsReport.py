1942cff7d952d6be6b548deb1cca26558750bd7e
slaney <93399147+slaneycoursera@users.noreply.github.com>
2024-01-08 08:46:30 -0700

    Update jamfProMetricsReport.py
    Adjusted the sort and made sure that there was a total count in the end.  Also adjusted from json to the expected xml that is recieved

--- Scripts/miscScripts/jamfProMetricsReport.py
+++ Scripts/miscScripts/jamfProMetricsReport.py
@@ -1,10 +1,11 @@
 import requests
+import sys
 import subprocess
 from getpass import getpass
+from xml.etree import ElementTree
 
 # read jamf url from system defaults
 myJssBaseurl = subprocess.getoutput("/usr/bin/defaults read /Library/Preferences/com.jamfsoftware.jamf jss_url")
-jamf_url = f"{myJssBaseurl}api/v1/jamf-management-framework/redeploy/"
 
 # Ask for bearer token
 bearer_token = getpass('Please enter bearer token: ')
@@ -15,33 +16,43 @@ headers = {'Authorization': 'Bearer %s' % bearer_token}
 smart_group_ids = ['296', '297', '298','299','377']
 
 def get_info_smartgroup(smart_group_id):
-    # your JAMF Pro end point - replace placeholder with actual endpoint
-    endpoint = f'{jamf_url}/JSSResource/advancedcomputersearches/id/{smart_group_id}'
+    # your JAMF Pro endpoint
+    print(f'{myJssBaseurl}JSSResource/computergroups/id/{smart_group_id}')
+    endpoint = f'{myJssBaseurl}JSSResource/computergroups/id/{smart_group_id}'
 
-    # send a GET request to the JAMF Pro end point
-    response = requests.get(endpoint, headers=headers)
+    # send a GET request to the JAMF Pro endpoint
+    try:
+        response = requests.get(endpoint, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print ("HTTP Error:", errh)
+        return None
 
-    # handle response
-    if response.status_code != 200:
-        print('Error with status {}: ', response.content )
-        sys.exit()
+    # get the XML response body
+    data = ElementTree.fromstring(response.content)
 
-    # get the JSON response body
-    data = response.json()
-    # We will get group name and computers count from response. 
-    group_name = data['name']
-    computers_count = len(data['computers'])
+    # We will get group name and computers count from response.
+    group_name = data.find("name").text
+    computers_count = len(data.findall(".//computer"))
 
     return (group_name, computers_count)
 
 if __name__ == "__main__":
     results = []
+    total_count = 0 
+
     for gid in smart_group_ids:
-        results.append(get_info_smartgroup(gid))
+        smartgroup_info = get_info_smartgroup(gid)
 
-    #Sort results by number of computers in DESC order
-    sorted_results = sorted(results, key=lambda x:x[1], reverse=True)
+        # handle case where get_info_smartgroup returns None
+        if smartgroup_info is not None:
+            results.append(smartgroup_info)
+            total_count += smartgroup_info[1]
 
+    # It's now the order they were checked, because we don't sort it anymore
     #Print the results
-    for res in sorted_results:
+    for res in results:
         print('Smart Group Name: {}, Number of Computers: {}'.format(res[0],res[1]))
+
+    # Print total count
+    print('Total Number of Computers: {}'.format(total_count))
