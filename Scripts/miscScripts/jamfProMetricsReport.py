3680016607d429cb1e22b2ba75c6249d8a2c59ef
slaney <93399147+slaneycoursera@users.noreply.github.com>
2024-01-08 08:19:33 -0700

    Update jamfProMetricsReport.py
    Adjusting to report smartgroup names and count for macOS versions in use

--- Scripts/miscScripts/jamfProMetricsReport.py
+++ Scripts/miscScripts/jamfProMetricsReport.py
@@ -1,6 +1,5 @@
 import requests
 import subprocess
-import json
 
 # read jamf url from system defaults
 myJssBaseurl = subprocess.getoutput("/usr/bin/defaults read /Library/Preferences/com.jamfsoftware.jamf jss_url")
@@ -10,10 +9,10 @@ jamf_url = f"{myJssBaseurl}api/v1/jamf-management-framework/redeploy/"
 bearer_token = '<bearer_token_here>'
 headers = {'Authorization': 'Bearer %s' % bearer_token}
 
-#Id of your smart group
-smart_group_id = '<smart_group_id_here>'
+#Your smart groups IDs
+smart_group_ids = ['<id1>', '<id2>', '<id3>',...] #Replace with your IDs
 
-def get_computers_in_smartgroup():
+def get_info_smartgroup(smart_group_id):
     # your JAMF Pro end point - replace placeholder with actual endpoint
     endpoint = f'{jamf_url}/JSSResource/advancedcomputersearches/id/{smart_group_id}'
 
@@ -27,13 +26,20 @@ def get_computers_in_smartgroup():
 
     # get the JSON response body
     data = response.json()
+    # We will get group name and computers count from response. 
+    group_name = data['name']
+    computers_count = len(data['computers'])
 
-    # We will check computers from response. 
-    computers = data.get('computers')
-
-    # print each computer's OS version
-    for computer in computers:
-        print("Computer with id: {} has OS version: {}".format(computer['id'], computer['os_version']))
+    return (group_name, computers_count)
 
 if __name__ == "__main__":
-    get_computers_in_smartgroup()
+    results = []
+    for gid in smart_group_ids:
+        results.append(get_info_smartgroup(gid))
+
+    #Sort results by number of computers in DESC order
+    sorted_results = sorted(results, key=lambda x:x[1], reverse=True)
+
+    #Print the results
+    for res in sorted_results:
+        print('Smart Group Name: {}, Number of Computers: {}'.format(res[0],res[1]))
